a6ca3c3af6cc5bcb9eff65c7f537a4b321d896cc
dmurambadoro <dmurambadoro@ymail.com>
2021-03-10 06:16:48 -0600

    Add humidity data to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,7 @@ const forecast = (latitude, longitude, callback) => {
             callback("Unable to find location", undefined)
         } else {
             //callback(undefined, response.body.daily.data[0].summary + " It is currently " + response.body.current.temperature + " degrees out. It feels like " + response.body.current.feelslike + " though!")
-            callback(undefined, " It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " though!")
+            callback(undefined, " It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " though! The humidity is " + body.current.humidity + "%")
 
         }
 })
